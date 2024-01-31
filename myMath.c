ff255e9ea95c7a0929833d8aff32ee05c7f0de93
thalesdemond <thalesdemond@yahoo.com.br>
2024-01-31 11:08:42 -0300

    Create myMath.c

---
+++ myMath.c
@@ -0,0 +1,10 @@
+#include <myMath.h>
+
+double evalPoly(double x, double * poly, unsigned int s){
+    double res = poly[0];
+    for(unsigned int i = 1; i < s; i++){
+        res *= x;
+        res += poly[i];
+    }
+    return (res);
+}
