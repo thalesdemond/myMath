a1601c754b5a6bfa2220f62ca2bcbfe9215cd36d
thalesdemond <thalesdemond@yahoo.com.br>
2024-01-31 11:11:51 -0300

    Update myMath.h

--- myMath.h
+++ myMath.h
@@ -1,1 +1,9 @@
-double evalPoly(double x, double * poly, unsigned int s)
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    extern double evalPoly(double x, double * poly, unsigned int s);
+
+#ifdef	__cplusplus
+}
+#endif
